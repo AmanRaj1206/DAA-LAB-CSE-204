82c94e103dff3dc7e2272c57ba28e506491c91ef
AmanRaj1206 <pinkiamar1976@gmail.com>
2026-08-16 05:06:19 +0530

    Update Summation of series Method 2.c

--- Exp-02/Summation of series Method 2.c
+++ Exp-02/Summation of series Method 2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main()
+{
+    double a, r, sum = 0;
+    int n;
+
+    printf("Enter the first term (a): ");
+    scanf("%lf", &a);
+
+    printf("Enter the common ratio (r): ");
+    scanf("%lf", &r);
+
+    printf("Enter the number of terms (n): ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + a;
+        a = a * r;
+    }
+
+    printf("Sum of geometric series = %.2lf\n", sum);
+
+    return 0;
+}
