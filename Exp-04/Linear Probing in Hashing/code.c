552cf10c6eb2f65b9fe6e2fe273c0c45f1a23f5d
AmanRaj1206 <pinkiamar1976@gmail.com>
2026-08-30 18:19:20 +0530

    Update code.c

--- Exp-04/Linear Probing in Hashing/code.c
+++ Exp-04/Linear Probing in Hashing/code.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+#define SIZE 10
+
+int main()
+{
+    int hashTable[SIZE];
+    int n, value, index;
+
+    for (int i = 0; i < SIZE; i++)
+        hashTable[i] = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+
+        index = value % SIZE;
+
+        while (hashTable[index] != -1)
+        {
+            index = (index + 1) % SIZE;
+        }
+
+        hashTable[index] = value;
+    }
+
+    printf("Hash Table using Linear Probing:\n");
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (hashTable[i] == -1)
+            printf("[%d] : Empty\n", i);
+        else
+            printf("[%d] : %d\n", i, hashTable[i]);
+    }
+
+    return 0;
+}
