ead7d565c69118407b17beb9d23f39c00d9a0eb8
AmanRaj1206 <pinkiamar1976@gmail.com>
2026-08-30 18:19:07 +0530

    Update code.c

--- Exp-04/Quadratic Probing in Hashing/code.c
+++ Exp-04/Quadratic Probing in Hashing/code.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+#define SIZE 10
+
+int main()
+{
+    int hashTable[SIZE];
+    int n, value, index, j;
+
+    for (int i = 0; i < SIZE; i++)
+        hashTable[i] = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+
+        j = 0;
+        index = value % SIZE;
+
+        while (hashTable[index] != -1)
+        {
+            j++;
+            index = (value % SIZE + j * j) % SIZE;
+        }
+
+        hashTable[index] = value;
+    }
+
+    printf("Hash Table using Quadratic Probing:\n");
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (hashTable[i] == -1)
+            printf("[%d] : Empty\n", i);
+        else
+            printf("[%d] : %d\n", i, hashTable[i]);
+    }
+
+    return 0;
+}
